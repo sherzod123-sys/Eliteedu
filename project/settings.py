7c70e7ee8e4b04b556bbfb738a9c6d79a6855032
qo-diraliyev <qodiraliyevsherzod431@gmail.com>
2026-06-13 14:27:31 +0500

    fix: complete settings fix for railway

--- project/settings.py
+++ project/settings.py
@@ -553,8 +553,6 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
-
-WHITENOISE_MANIFEST_STRICT = False
