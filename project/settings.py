9bea8a33dc9ef8afbb1828e9ae7da0c195c52994
qo-diraliyev <qodiraliyevsherzod431@gmail.com>
2026-06-13 13:07:34 +0500

    fix: static files config

--- project/settings.py
+++ project/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-# STATICFILES_DIRS = [BASE_DIR / 'static']
+# STATICFILES_DIRS = []
 
 # Media files
 MEDIA_URL = '/media/'
