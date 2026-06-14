cf1a99c5317bc1a1ca7dfa0ea580a69d15821f7c
qo-diraliyev <qodiraliyevsherzod431@gmail.com>
2026-06-14 15:16:41 +0500

    Fix: Redis channel layer capacity optimization

--- backend/backend/project/settings.py
+++ backend/backend/project/settings.py
@@ -106,7 +106,9 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)],
+            'hosts': [os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379')],
+            'capacity': 1500,
+            'expiry': 10,
         },
     },
 }
